78c7d8412d4b761691292265ae30bfbf075b5802
vvsosed <volodymyr.sosedkin@ezlo.com>
2021-06-02 13:51:04 +0300

    DecoderBinTo7SegHex example

--- 04_Decoder7Seg_4/DecoderBinTo7SegHex.v
+++ 04_Decoder7Seg_4/DecoderBinTo7SegHex.v
@@ -1,22 +1,22 @@
 /*
     -Segments of indicator-
     
-             A
-           -----
+             
+          -- A --
           |     |
-        F |     | B
-          |  G  |
-          -----
+          F     B
           |     |
-        E |     | C
-          |  D  |
-           -----  (dp)
+          -- G --
+          |     |
+          E     C
+          |     |
+          -- D --  (DP)
 
 
     /----------------------\
     |  -Truth table-       |
     >----------------------<
-    | Input:|    ABCDEFG   |
+    | Input:|     ABCDEFG  |
     >----------------------<
     | 4'h0 --> 7'b1111110; |
     | 4'h1 --> 7'b0110000; |
@@ -26,46 +26,45 @@
     | 4'h5 --> 7'b1011011; |
     | 4'h6 --> 7'b1011111; |
     | 4'h7 --> 7'b1110000; |
-    | 4'h8 --> 7'bxxxxxxx; |
-    | 4'h9 --> 7'bxxxxxxx; |
-    | 4'hA --> 7'bxxxxxxx; |
-    | 4'hB --> 7'bxxxxxxx; |
-    | 4'hC --> 7'bxxxxxxx; |
-    | 4'hD --> 7'bxxxxxxx; |
-    | 4'hE --> 7'bxxxxxxx; |
-    | 4'hF --> 7'bxxxxxxx; |
+    | 4'h8 --> 7'b1111111; |
+    | 4'h9 --> 7'b1111011; |
+    | 4'hA --> 7'b1110111; |
+    | 4'hB --> 7'b0011111; |
+    | 4'hC --> 7'b1001110; |
+    | 4'hD --> 7'b0111101; |
+    | 4'hE --> 7'b1001111; |
+    | 4'hF --> 7'b1000111; |
     \----------------------/
 
 */
 
 module DecoderBinTo7SegHex(
-    output wire [7:0] fullOut,
+    output wire [7:0] segOut, // [A..F]
     input  wire [3:0] valIn,
     input  wire       dpIn
 );
-    reg [6:0] segOut;
+    reg [6:0] segs;
     
-    assign fullOut = { dpIn, segOut };
+    assign segOut = { dpIn, segs };
     
     always @( valIn )
       case( valIn )
-          4'h0: segOut = 7'b1111110;
-          4'h1: segOut = 7'b0110000;
-          4'h2: segOut = 7'b1101101;
-          4'h3: segOut = 7'b1111001;
-          4'h4: segOut = 7'b0110011;
-          4'h5: segOut = 7'b1011011;
-          4'h6: segOut = 7'b1011111;
-          4'h7: segOut = 7'b1110000;
-
-          4'h8: segOut = 7'b0000000;
-          4'h9: segOut = 7'b0000100;
-          4'hA: segOut = 7'b0001000;
-          4'hB: segOut = 7'b1100000;
-          4'hC: segOut = 7'b0110001;
-          4'hD: segOut = 7'b1000010;
-          4'hE: segOut = 7'b0110000;
-          default: segOut = 7'b0111000; // 4'hF
+          4'h0: segs = ~7'b1111110;
+          4'h1: segs = ~7'b0110000;
+          4'h2: segs = ~7'b1101101;
+          4'h3: segs = ~7'b1111001;
+          4'h4: segs = ~7'b0110011;
+          4'h5: segs = ~7'b1011011;
+          4'h6: segs = ~7'b1011111;
+          4'h7: segs = ~7'b1110000;
+          4'h8: segs = ~7'b1111111;
+          4'h9: segs = ~7'b1111011;
+		  4'hA: segs = ~7'b1110111;
+		  4'hB: segs = ~7'b0011111;
+          4'hC: segs = ~7'b1001110;
+		  4'hD: segs = ~7'b0111101;
+		  4'hE: segs = ~7'b1001111;
+          default: segs = ~7'b1000111; // 4'hF
       endcase
 
 endmodule // module Decoder7Seg
--- 04_Decoder7Seg_4/RunningCounter.v
+++ 04_Decoder7Seg_4/RunningCounter.v
@@ -2,34 +2,37 @@
 module RunningCounter( output wire [7:0] segOut, 
                        output wire [3:0] digitOut, 
 							  input wire clock, 
-							  input wire rst 
+							  input wire reset 
 );
 	reg [36:0] delayCtr;
 	reg [1:0] digCtr;
 	
-	wire [7:0] segIn;
+	wire [3:0] valIn;
 	DecoderBinTo7SegHex decSeg (
-		.fullOut( segOut ),
-		.valIn( segIn ),
-		.dpIn( 0 )
+		.segOut( segOut ),
+		.valIn( valIn ),
+		.dpIn( 1'b1 )
 	);
-		
-	assign digitOut[0] = (digCtr == 0);
-	assign digitOut[1] = (digCtr == 1);
-	assign digitOut[2] = (digCtr == 2);
-	assign digitOut[3] = (digCtr == 3);
 	
-	assign segIn = (digCtr == 0) ? delayCtr[24:21] :
+	assign digitOut[0] = !reset || (digCtr != 0);
+	assign digitOut[1] = !reset || (digCtr != 1);
+	assign digitOut[2] = !reset || (digCtr != 2);
+	assign digitOut[3] = !reset || (digCtr != 3);
+	
+	assign valIn = (digCtr == 0) ? delayCtr[24:21] :
                   (digCtr == 1) ? delayCtr[28:25] :
                   (digCtr == 2) ? delayCtr[32:29] :
                 /*(digCtr == 3)*/ delayCtr[36:33];
 	
 	always @ ( posedge clock ) begin 
-		delayCtr = delayCtr + 1;
+		if ( !reset )
+			delayCtr = 37'h0;
+		else
+			delayCtr = delayCtr + 1'b1;
 	end
 	
-	always @ ( delayCtr[15] ) begin
-		digCtr = digCtr + 1;
+	always @ ( posedge delayCtr[15] ) begin
+		digCtr = digCtr + 1'b1;
 	end
 	
 endmodule // RunningCounter
